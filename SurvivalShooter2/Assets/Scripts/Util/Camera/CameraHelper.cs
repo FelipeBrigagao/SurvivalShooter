90dd49f4e9906504581eb37c6edd457f79046c91
FelipeBrigagao <38021142+FelipeBrigagao@users.noreply.github.com>
2021-12-08 21:11:36 -0300

    Replaced the normal camera with cinemachine

---
+++ SurvivalShooter2/Assets/Scripts/Util/Camera/CameraHelper.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+public class CameraHelper : MonoBehaviour
+{
+    #region Variables
+    [SerializeField] private CinemachineVirtualCamera _cam;
+    #endregion
+
+    #region Unity Methods
+    private void Awake()
+    {
+        PlayerManager.Instance.SetPlayerCam(_cam);
+    }
+    #endregion
+}
